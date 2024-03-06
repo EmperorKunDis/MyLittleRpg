b95c1b7df334f65d54b8fe21f989e205a43dd10b
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-03-06 14:56:45 +0000

    Author:    Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   MyLittleRpg/src/Char.h

--- MyLittleRpg/src/Char.h
+++ MyLittleRpg/src/Char.h
@@ -7,14 +7,83 @@
     #include "Stats.h"
     #include "Equip.h"
 
-    class Character {
+    class Character 
+    {
+
+    private:
+        
+        const std::vector<std::wstring> m_EquipmentSlotLabels = 
+        { 
+            L"Head",
+            L"Body",
+            L"Shoulders",
+            L"Tabard",
+            L"Legs",
+            L"Feet",
+            L"Left Hand", L"Right Hand",
+            L"First Ring", L"Second Ring", L"Third Ring", L"Fourth Ring",
+            L"Neck",
+            L"Trinket" 
+        };
+
+        const std::vector<std::wstring> m_StatsLabels = 
+        {
+            L"Attack",
+            L"Defence",
+            L"Speed",
+            L"Intelligence",
+            L"Strength",
+            L"Dexterity",
+            L"Stamina"
+        };
+        
+    protected:
+        std::wstring m_Name;
+        std::wstring m_Identity;
+        int m_Level;
+
+        int m_MaxHP;
+        int m_CurrentHP;
+
+        int m_XPToNextLevel;
+        int m_XP;
+
+        int m_MaxMP;
+        int m_CurrentMP;
+    
     public:
+
+        enum class EntityType
+        {
+            Player,
+            Skeleton,
+            Goblin,
+            Bandit,
+            Werewolf,
+            Troll,
+            Necromancer,
+            Vampire,
+            Dragon,
+            Devil
+        };
+
+        enum class CharType
+        {
+            Warrior,
+            Thief,
+            Mage,
+            Cleric,
+            Paladin,
+            Necromancer,
+            Ranger,
+            Wizard
+        };
         
         void move();
         void attack();
-        std::vector<std::string> enemyType{"Skeleton", "Goblin", "Ogre", "Werewolf", "Troll", "Orc", "Vampire", "Dragon"};
-        // Other character actions and attributes
-        void getHealthByType(health);
-    private:
-        
+        void takeDamage();
+
+        Character();
+        Character(const std::wstring& name, const std::wstring& identity, int level, int max_hp, CharType type = CharType::Mage);
+        ~Character() = default;
     };
