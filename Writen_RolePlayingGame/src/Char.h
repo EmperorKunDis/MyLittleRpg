253bdb203b50f4be8925ebb279ffc9b200a094ae
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-03-06 09:08:18 +0000

    Date:      Wed Mar 6 09:08:18 2024 +0000
    On branch main
    Your branch is up to date with 'origin/dev'.
    
    Changes to be committed:
    	new file:   .vscode/c_cpp_properties.json
    	new file:   .vscode/launch.json
    	new file:   .vscode/settings.json
    	new file:   Writen_RolePlayingGame/.vscode/c_cpp_properties.json
    	new file:   Writen_RolePlayingGame/.vscode/launch.json
    	new file:   Writen_RolePlayingGame/.vscode/settings.json
    	new file:   Writen_RolePlayingGame/assets/ArmorDefs.xml
    	new file:   Writen_RolePlayingGame/assets/ArmorShopDef_1.xml
    	new file:   Writen_RolePlayingGame/assets/ItemDefs.xml
    	new file:   Writen_RolePlayingGame/assets/WeaponDefs.xml
    	new file:   Writen_RolePlayingGame/assets/WeaponShopDef_1.xml
    	new file:   Writen_RolePlayingGame/src/Char.cpp
    	new file:   Writen_RolePlayingGame/src/Char.h
    	new file:   Writen_RolePlayingGame/src/Equip.h
    	new file:   Writen_RolePlayingGame/src/Game.cpp
    	new file:   Writen_RolePlayingGame/src/Game.h
    	new file:   Writen_RolePlayingGame/src/Stats.h

---
+++ Writen_RolePlayingGame/src/Char.h
@@ -0,0 +1,20 @@
+    // Char.h
+    #pragma once
+    #include <string>
+    #include <vector>
+    #include <unordered_map>
+    #include <memory>
+    #include "Stats.h"
+    #include "Equip.h"
+
+    class Character {
+    public:
+        
+        void move();
+        void attack();
+        std::vector<std::string> enemyType{"Skeleton", "Goblin", "Ogre", "Werewolf", "Troll", "Orc", "Vampire", "Dragon"};
+        // Other character actions and attributes
+        void getHealthByType(health);
+    private:
+        
+    };
---
+++ Writen_RolePlayingGame/src/Game.cpp
@@ -0,0 +1,19 @@
+// Game.cpp
+#include "Game.h"
+
+Game::Game() {
+    // Constructor implementation
+}
+void Game::start() {
+    // Start implementation
+}
+void Game::update() {
+    randNum() % 10 + 1; 
+    // Update implementation
+}
+void Game::render() {
+    // Render implementation
+}
+void Game::end() {
+    // End implementation
+}
