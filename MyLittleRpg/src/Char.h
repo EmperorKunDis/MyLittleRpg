d565325b9e30c0b9f537cb670d66ae73e1f0f7c6
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-03-06 15:42:33 +0000

    Author:    Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com> On branch main Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.  (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   MyLittleRpg/src/Char.h
    	modified:   MyLittleRpg/src/Game.cpp
    	modified:   MyLittleRpg/src/Game.h

--- MyLittleRpg/src/Char.h
+++ MyLittleRpg/src/Char.h
@@ -7,83 +7,66 @@
     #include "Stats.h"
     #include "Equip.h"
 
-    class Character 
+    class Char
     {
-
-    private:
-        
-        const std::vector<std::wstring> m_EquipmentSlotLabels = 
-        { 
-            L"Head",
-            L"Body",
-            L"Shoulders",
-            L"Tabard",
-            L"Legs",
-            L"Feet",
-            L"Left Hand", L"Right Hand",
-            L"First Ring", L"Second Ring", L"Third Ring", L"Fourth Ring",
-            L"Neck",
-            L"Trinket" 
+    public:
+        enum class EntityType
+        {
+            Player, Skeleton, Goblin, Bandit, Werewolf, Troll, Necromancer, Vampire, Dragon, Devil
         };
-
+        enum class CharType
+        {
+            Warrior, Thief, Mage, Cleric, Paladin, Necromancer, Ranger, Wizard
+        };
+    private:
+        const std::vector<std::wstring> m_EquipSlotsLabels = 
+        { 
+            L"Head", L"Body", L"Shoulders", L"Tabard", L"Legs", L"Feet", L"Left Hand", L"Right Hand",
+            L"First Ring", L"Second Ring", L"Third Ring", L"Fourth Ring", L"Neck", L"Trinket" 
+        };
         const std::vector<std::wstring> m_StatsLabels = 
         {
-            L"Attack",
-            L"Defence",
-            L"Speed",
-            L"Intelligence",
-            L"Strength",
-            L"Dexterity",
-            L"Stamina"
+            L"Attack", L"Defence", L"Speed", L"Intelligence", L"Strength", L"Dexterity", L"Stamina"
         };
         
     protected:
         std::wstring m_Name;
         std::wstring m_Identity;
         int m_Level;
+        int m_MaxHP, m_CurrentHP;
+        int m_XPToNextLevel, m_XP;
+        int m_MaxMP, m_CurrentMP;
+        bool m_bDead;
 
-        int m_MaxHP;
-        int m_CurrentHP;
+	    CharType m_eCharType;
+	
+	    std::unordered_map<Stats::EquipSlots, std::shared_ptr<Equip>> m_mapEquipSlots;
 
-        int m_XPToNextLevel;
-        int m_XP;
+        Stats m_Stats;
 
-        int m_MaxMP;
-        int m_CurrentMP;
-    
+
     public:
-
-        enum class EntityType
-        {
-            Player,
-            Skeleton,
-            Goblin,
-            Bandit,
-            Werewolf,
-            Troll,
-            Necromancer,
-            Vampire,
-            Dragon,
-            Devil
-        };
-
-        enum class CharType
-        {
-            Warrior,
-            Thief,
-            Mage,
-            Cleric,
-            Paladin,
-            Necromancer,
-            Ranger,
-            Wizard
-        };
-        
         void move();
         void attack();
         void takeDamage();
-
-        Character();
-        Character(const std::wstring& name, const std::wstring& identity, int level, int max_hp, CharType type = CharType::Mage);
+        Char();
+        Char(const std::wstring& name, const std::wstring& identity, int level, int max_hp, CharType type = CharType::Mage);
         ~Character() = default;
+
+        inline const int GetHP() const { return m_CurrentHP; }
+        inline const int GetMaxHP() const { return m_MaxHP; }
+        inline const int GetMP() const { return m_CurrentMP; }
+        inline const int GetMaxMP() const { return m_MaxMP; }
+        inline const int GetXP() const { return m_XP; }
+        inline const int GetXPToNextLevel() const { return m_XPToNextLevel; }
+        
+        inline const std::wstring& GetName() const { return m_Name; }
+        inline const std::wstring& GetIdentity() const { return m_Identity; }
+        inline const int GetLevel() const { return m_Level; }
+        
+        const std::vector<std::wstring>& GetEquipmentSlotLabels() const { return m_EquipmentSlotLabels; }
+        const std::vector<std::wstring>& GetStatsLabels() const { return m_StatsLabels; }
+
+        std::unordered_map<Stats::EquipSlots, std::shared_ptr<Equip>>& GetEquipment() { return m_Equipment; }
+        Stats& GetStats() const { return m_StatsLabels; }
     };
--- MyLittleRpg/src/Game.h
+++ MyLittleRpg/src/Game.h
@@ -1,12 +1,33 @@
-// Game.h
 #pragma once
+#include "Console.h"
+#include "inputs/Keyboard.h"
+#include "states/StateMachine.h"
 
-class Game {
+class Game
+{
+private:
+	bool m_bIsRunning;
+	
+	std::unique_ptr<Console> m_pConsole;
+	std::unique_ptr<Keyboard> m_pKeyboard;
+	std::unique_ptr<StateMachine> m_pStateMachine;
+
+	DWORD m_NumRead;
+	INPUT_RECORD m_InRecBuf[128];
+	HANDLE m_hConsoleIn;
+	
+	bool Init();
+
+	void ProcessEvents();
+	void ProcessInputs();
+	void Update();
+	void Draw();
+
+	void KeyEventProcess(KEY_EVENT_RECORD keyEvent);
+
 public:
-    Game();
-    void start();
-    void update();
-    void render();
-    void end();
-    int randNum();
+	Game(); 
+	~Game();
+
+	void Run();
 };
