d4883c3d14eec6d334019cd24f9e8d10540e82d2
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-03-07 13:10:13 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Author:    Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   MyLittleRpg/src/Char.cpp
        modified:   MyLittleRpg/src/Char.h

--- MyLittleRpg/src/Char.cpp
+++ MyLittleRpg/src/Char.cpp
@@ -1,23 +1,55 @@
+#include "Char.h"
 
-    #include "Char.h"
-   Character::Character() {
+Char::Char()
 
-        int getHealthByType(health) {
-            if (enemyType == "Skeleton") {return 5;} 
-            else if (enemyType == "Goblin") {return 10;}
-            else if (enemyType == "Ogre") {return 15;}
-            else if (enemyType == "Werewolf") {return 20;}
-            else if (enemyType == "Troll") {return 25;}
-            else if (enemyType == "Orc") {return 30;
+    : Char(L"m_sName", L"Char_Identity", 0, 100, ClassType::Necromancer)
+{
+
 }
-            else if (enemyType == "Vampire") {
-                return 35;
-            }
-            else if (enemyType == "Dragon") {
-                return 100;
-            }
-            else {
-                return 0;
-            }
-        };
-    };
+
+Char::Char(std::wstring name, std::wstring identity, int level,int currentXP, int XPToNextLevel, int currentHP, int currentMP, int maxMP, int maxHP, ClassType type)
+    {   m_sName{name},  
+        m_sIdentity{identity}, 
+        m_Level{level}, 
+        m_CurrentXP{0},
+        m_XPToNextLevel{50},
+        m_MaxHP{maxHP},}
+        m_MaxMP{0}, 
+        m_CurrentMP{0},
+        m_currentStamina{0},
+        m_maxStamina{0},
+        m_attackPower{0},
+        m_defencePower{0},
+        m_speedPower{0},
+        m_luckyPower{0},
+        m_charismaPower{0},
+        m_magicPower{0},
+        m_bDead{false},
+        m_eCharType{type},
+        m_mapEquipSlots{
+            { Stats::EquipSlot::Head, nullptr},
+            { Stats::EquipSlot::Body, nullptr},
+            { Stats::EquipSlot::Shoulders, nullptr},
+            { Stats::EquipSlot::Tabard, nullptr},
+            { Stats::EquipSlot::Legs, nullptr},
+            { Stats::EquipSlot::Feet, nullptr},
+            { Stats::EquipSlot::LeftHand, nullptr},
+            { Stats::EquipSlot::RightHand, nullptr},
+            { Stats::EquipSlot::FirstRing, nullptr},
+            { Stats::EquipSlot::SecondRing, nullptr},
+            { Stats::EquipSlot::ThirdRing, nullptr},
+            { Stats::EquipSlot::FourthRing, nullptr},
+            { Stats::EquipSlot::Neck, nullptr},
+            { Stats::EquipSlot::Trinket, nullptr}
+        } ,
+        m_mapStats{5, 2, 3, 5, 4, 6, 7}
+    }                       //Just some random numbers to test stats Original is 1, 1, 1, 1, 1, 1, 1, on level 0
+
+{
+
+}
+
+void Char::HealHP(int holyPower)
+    {
+        m_CurrentHP += holyPower;
+    }
--- MyLittleRpg/src/Char.h
+++ MyLittleRpg/src/Char.h
@@ -14,59 +14,92 @@
         {
             Player, Skeleton, Goblin, Bandit, Werewolf, Troll, Necromancer, Vampire, Dragon, Devil
         };
-        enum class CharType
+        enum class ClassType
         {
-            Warrior, Thief, Mage, Cleric, Paladin, Necromancer, Ranger, Wizard
+            Warrior, Thief, Mage, Cleric, Paladin, Necromancer, Ranger
         };
     private:
-        const std::vector<std::wstring> m_EquipSlotsLabels = 
+        const std::vector<std::wstring> m_EquipSlotLabel = 
         { 
-            L"Head", L"Body", L"Shoulders", L"Tabard", L"Legs", L"Feet", L"Left Hand", L"Right Hand",
-            L"First Ring", L"Second Ring", L"Third Ring", L"Fourth Ring", L"Neck", L"Trinket" 
+            L"Head", L"Body", L"Shoulders", L"Tabard", L"Legs", L"Feet", L"Left Hand", L"Right Hand",       // Head Body Shoulders Tabard Legs Feet Left_Hand Right_Hand
+            L"First Ring", L"Second Ring", L"Third Ring", L"Fourth Ring", L"Neck", L"Trinket"               // First - Fourth Ring Neck Trinket
         };
-        const std::vector<std::wstring> m_StatsLabels = 
+        const std::vector<std::wstring> m_StatsLabel = 
         {
-            L"Attack", L"Defence", L"Speed", L"Intelligence", L"Strength", L"Dexterity", L"Stamina"
+            L"Strength", L"Dexterity", L"Vitality" ,L"Intelligence", L"Wisdom", L"Charisma", L"Luck"
         };
         
     protected:
-        std::wstring m_Name;
-        std::wstring m_Identity;
+        std::wstring m_sName;
+        std::wstring m_sIdentity;
         int m_Level;
-        int m_MaxHP, m_CurrentHP;
-        int m_XPToNextLevel, m_XP;
-        int m_MaxMP, m_CurrentMP;
+        int m_MaxHP,            m_CurrentHP;
+        int m_XPToNextLevel,    m_CurrentXP;
+        int m_MaxMP,            m_CurrentMP;
+        int m_currentStamina,   m_maxStamina;
+        int m_attackPower,      m_defencePower,     m_speedPower,   m_staminaPower,     m_luckyPower,    m_charismaPower,   m_magicPower;
         bool m_bDead;
 
-	    CharType m_eCharType;
+	    ClassType m_eClassType;
 	
-	    std::unordered_map<Stats::EquipSlots, std::shared_ptr<Equip>> m_mapEquipSlots;
+	    std::unordered_map<Stats::EquipSlot, std::shared_ptr<Equip>> m_mapEquipSlot;
 
         Stats m_Stats;
 
 
     public:
-        void move();
-        void attack();
-        void takeDamage();
         Char();
-        Char(const std::wstring& name, const std::wstring& identity, int level, int max_hp, CharType type = CharType::Mage);
-        ~Character() = default;
+        Char(const std::wstring& name, const std::wstring& identity, int level, int max_hp, ClassType type = ClassType::Necromancer);
 
-        inline const int GetHP() const { return m_CurrentHP; }
-        inline const int GetMaxHP() const { return m_MaxHP; }
-        inline const int GetMP() const { return m_CurrentMP; }
-        inline const int GetMaxMP() const { return m_MaxMP; }
-        inline const int GetXP() const { return m_XP; }
-        inline const int GetXPToNextLevel() const { return m_XPToNextLevel; }
-        
-        inline const std::wstring& GetName() const { return m_Name; }
-        inline const std::wstring& GetIdentity() const { return m_Identity; }
-        inline const int GetLevel() const { return m_Level; }
-        
-        const std::vector<std::wstring>& GetEquipmentSlotLabels() const { return m_EquipmentSlotLabels; }
-        const std::vector<std::wstring>& GetStatsLabels() const { return m_StatsLabels; }
+        ~Char() = default;
 
-        std::unordered_map<Stats::EquipSlots, std::shared_ptr<Equip>>& GetEquipment() { return m_Equipment; }
-        Stats& GetStats() const { return m_StatsLabels; }
+        const std::vector<std::wstring>& GetEquipSlotLabel() const { return m_EquipSlotLabel; }
+        const std::vector<std::wstring>& GetStatsLabel() const { return m_StatsLabel; }
+
+        std::unordered_map<Stats::EquipSlot, std::shared_ptr<Equip>>& GetEquipment() { return m_Equip; }
+        Stats& GetStats() { return m_Stats; }
+        
+        inline const std::wstring& GetName() const { return m_sName; }
+        inline const std::wstring& GetIdentity() const { return m_sIdentity; }
+
+        const int GetLevel() const { return m_Level; }
+        const int GetHP() const { return m_CurrentHP; }
+        const int GetMaxHP() const { return m_MaxHP; }
+        const int GetMP() const { return m_CurrentMP; }
+        const int GetMaxMP() const { return m_MaxMP; }
+        const int GetXP() const { return m_CurrentXP; }
+        const int GetStamina() const { return m_currentStamina; }
+        const int GetMaxStamina() const { return m_maxStamina; }
+        const int GetAttackPower() const { return m_attackPower; }
+        const int GetDefencePower() const { return m_defencePower; }
+        const int GetSpeedPower() const { return m_speedPower; }
+        const int GetStaminaPower() const { return m_staminaPower; }
+        const int GetLuckyPower() const { return m_luckyPower; }
+        const int GetCharismaPower() const { return m_charismaPower; }
+        const int GetMagicPower() const { return m_magicPower; }
+
+
+        const int GetXPToNextLevel() const { return m_XPToNextLevel; }
+
+
+        const bool IsDead() const { return m_bDead; }
+
+        void HealHP(int holyPower);
+
+
+        void VampHP(int unholyPower);
+
+
+        void Move(int distance);
+
+
+        void Attack(int attackDamage);
+
+
+        void TakeDamage(int damage);
+
+
+        bool UseMagic(int mp);
+
+
     };
